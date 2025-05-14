2c8258deb29dc642e6a1e9aa84e8b8c9189a3952
Diego <dsantacruz@unsa.edu.pe>
2025-05-14 16:19:49 -0500

    Agregando metodo para ordenar el arreglo y usando esto para encontrar la mediana

--- LaboratorioEDA1/Ejercicio1.java
+++ LaboratorioEDA1/Ejercicio1.java
@@ -13,7 +13,13 @@ class Main {
     for(int nota : lista){
         System.out.print("["+nota+"] ");    
     }
-    System.out.print("\nLa moda es: "+moda(lista));
+    System.out.println("\nLa moda es: "+moda(lista));
+
+    
+    for(int nota: ordenar(lista)){
+        System.out.print("["+nota+"] ");    
+    }
+        System.out.print("\nLa mediana es: "+mediana(ordenar(lista)));
     }
 
     public static int moda(int[] lista){
@@ -32,4 +38,37 @@ class Main {
         }
         return moda;
     }
+    // Metodo Burbuja
+    public static int[] ordenar(int[] lista) {
+        // Crear una nueva lista para no modificar la original
+        int[] listaOrdenada = Arrays.copyOf(lista, lista.length);
+
+        // Ordenamiento por burbuja
+        for (int i = 0; i < listaOrdenada.length - 1; i++) {
+            for (int j = 0; j < listaOrdenada.length - 1 - i; j++) {
+                if (listaOrdenada[j] > listaOrdenada[j + 1]) {
+                    // Intercambiar los valores
+                    int temp = listaOrdenada[j];
+                    listaOrdenada[j] = listaOrdenada[j + 1];
+                    listaOrdenada[j + 1] = temp;
+                }
+            }
+        }
+
+        return listaOrdenada;
+    }
+
+    // Metodo para calcular la mediana
+    public static double mediana(int[] lista) {
+        int n = lista.length;
+        if (n % 2 != 0) {
+            // Si el número de elementos es impar, la mediana es el valor central
+            return lista[n / 2];
+        } else {
+            // Si el número de elementos es par, la mediana es el promedio de los dos valores centrales
+            return (lista[n / 2 - 1] + lista[n / 2]) / 2.0;
+        }
+    }
+
+
 }
