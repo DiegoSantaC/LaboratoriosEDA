0c473330f3fde506cd2be64549c31a358cccc6b2
Diego <dsantacruz@unsa.edu.pe>
2025-05-14 16:41:49 -0500

    Creando el metodo de la media y usandolo para la desviacion estandar

--- LaboratorioEDA1/Ejercicio1.java
+++ LaboratorioEDA1/Ejercicio1.java
@@ -19,8 +19,11 @@ class Main {
     for(int nota: ordenar(lista)){
         System.out.print("["+nota+"] ");    
     }
-        System.out.print("\nLa mediana es: "+mediana(ordenar(lista)));
+    System.out.print("\nLa mediana es: "+mediana(ordenar(lista)));
+    System.out.print("\nLa media es: "+media(lista));
+    System.out.print("\nLa desviacion estandar es: "+desviacionEstandar(lista));
     }
+
 
     public static int moda(int[] lista){
         int moda=0;
@@ -38,16 +41,13 @@ class Main {
         }
         return moda;
     }
-    // Metodo Burbuja
-    public static int[] ordenar(int[] lista) {
-        // Crear una nueva lista para no modificar la original
-        int[] listaOrdenada = Arrays.copyOf(lista, lista.length);
 
+    public static int[] ordenar(int[] lista) {
+        int[] listaOrdenada = Arrays.copyOf(lista, lista.length);
         // Ordenamiento por burbuja
         for (int i = 0; i < listaOrdenada.length - 1; i++) {
             for (int j = 0; j < listaOrdenada.length - 1 - i; j++) {
                 if (listaOrdenada[j] > listaOrdenada[j + 1]) {
-                    // Intercambiar los valores
                     int temp = listaOrdenada[j];
                     listaOrdenada[j] = listaOrdenada[j + 1];
                     listaOrdenada[j + 1] = temp;
@@ -58,16 +58,32 @@ class Main {
         return listaOrdenada;
     }
 
-    // Metodo para calcular la mediana
     public static double mediana(int[] lista) {
         int n = lista.length;
         if (n % 2 != 0) {
-            // Si el número de elementos es impar, la mediana es el valor central
             return lista[n / 2];
         } else {
-            // Si el número de elementos es par, la mediana es el promedio de los dos valores centrales
             return (lista[n / 2 - 1] + lista[n / 2]) / 2.0;
         }
+    }
+
+    public static double desviacionEstandar(int[] lista) {
+    double media = media(lista);
+    double sumaDiferencias = 0;
+
+    for (int nota : lista) {
+        sumaDiferencias += Math.pow(nota - media, 2);
+    }
+
+    return Math.sqrt(sumaDiferencias / lista.length);
+    }   
+
+    public static double media(int[] lista) {
+    double suma = 0;
+    for (int nota : lista) {
+        suma += nota;
+    }
+    return suma / lista.length;
     }
 
 
