39b635ff51b64767b2879da02daa6091877da466
Diego <dsantacruz@unsa.edu.pe>
2025-05-17 17:09:55 -0500

    Agregando ejercicio propuesto 6

--- Laboratorio2EDA/EjercicioP5.java
+++ Laboratorio2EDA/EjercicioP5.java
@@ -13,17 +13,17 @@ public class EjercicioP5 {
         int col=0;
         public void trianguloRecursivo3(int n){
         int anchoTotal=n*2-1;
-        if (fila <= n) {
+        if (fila<=n) {
             if (col >= anchoTotal) {
                 System.out.println();
                 fila++;
                 col = 0;
                 trianguloRecursivo3(n);
             } else {
-                int inicio = (anchoTotal - (fila * 2 - 1)) / 2;
-                if (col < inicio) {
+                int inicio = (anchoTotal-(fila*2-1))/2;
+                if (col<inicio) {
                     System.out.print(" ");
-                } else if ((col - inicio) % 2 == 0 && (col - inicio) / 2 < fila) {
+                } else if ((col-inicio)%2==0 && (col-inicio)/2<fila) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
---
+++ Laboratorio2EDA/EjercicioP6.java
@@ -0,0 +1,32 @@
+
+package com.mycompany.Laboratorio2EDA;
+
+public class EjercicioP6 {
+    
+    public static void main(String[] ar) {
+        Recursividad1 r=new Recursividad1();
+        r.cuadradoRecursivo(10);
+    }
+        
+    static class Recursividad1{
+        int fila=0;
+        int col=0;
+        public void cuadradoRecursivo(int n){
+            if(fila<n){           
+                if(col>=n){
+                    System.out.println();
+                    fila++;
+                    col=0;
+                    cuadradoRecursivo(n);
+                } else {
+                    if(fila==0||fila==n-1||col==0||col==n-1)
+                        System.out.print("*");
+                    else 
+                        System.out.print(" "); 
+                }
+                col++;
+                cuadradoRecursivo(n);
+            }             
+        }  
+    }
+}
