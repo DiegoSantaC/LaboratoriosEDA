3b76739b9585a9ce667c3a3b4c3de8d97e56af53
Diego <dsantacruz@unsa.edu.pe>
2025-05-17 13:05:38 -0500

    Creando clase y metodo del ejercicio 1 primera version

--- Laboratorio2EDA/EjercicioP1.java
+++ Laboratorio2EDA/EjercicioP1.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.Laboratorio2EDA;
 
-/**
- *
- * @author windows
- */
+package com.mycompany.Laboratorio2EDA;
+import java.util.*;
+
 public class EjercicioP1 {
+    public static void main(String[] ar) {
+        int[] vec1 = { 10,9,8,7,6,5,4,3,2,1};
+        Recursividad1 r=new Recursividad1();
+        r.imprimir(vec1);
+        int[]vec2 = r.invertirArray(vec1);
+        r.imprimir(vec2);
+    }
     
+    static class Recursividad1{
+        int cantidad=0;
+        int[] B;
+        
+        public int[] invertirArray(int[] A){
+            if(cantidad==0)
+                B=Arrays.copyOf(A, A.length);
+            
+            if(cantidad<B.length/2){
+                int temp=B[cantidad];
+                B[cantidad]=B[A.length-1-cantidad];
+                B[A.length-1-cantidad]=temp;
+                cantidad++;
+                invertirArray(B);
+            }
+            return B;
+        }
+        
+        void imprimir(int[] vec) {
+            for (int f = 0; f < vec.length; f++)
+                System.out.print(vec[f] + " ");
+            System.out.println("\n");
+        }
+    }
 }
