1cbed8cc690dc4b1e3533c3c926446463f69b75b
Diego <dsantacruz@unsa.edu.pe>
2025-05-24 19:09:20 -0500

    Agregando funcion para ejecutar las funciones usando un switch y validando para tipo de dato integer o double

--- laboratorio3eda/Ejercicio2/Main2.java
+++ laboratorio3eda/Ejercicio2/Main2.java
@@ -46,8 +46,36 @@ public class Main2 {
             System.out.println("8++. Salir");
             System.out.print("Elige una opcion: ");
             opcion = scanner.nextInt();
+            
+            if (opcion >= 1 && opcion <= 5) {
+                System.out.print("Tipo (1 = Integer, 2 = Double): ");
+                int tipo = scanner.nextInt();
+                if (tipo == 1) {
+                    System.out.print("Ingrese valor 1: ");
+                    Integer v1 = scanner.nextInt();
+                    System.out.print("Ingrese valor 2: ");
+                    Integer v2 = scanner.nextInt();
+                    ejecutarOperacion(opcion, v1, v2);
+                } else if (tipo == 2) {
+                    System.out.print("Ingrese valor 1: ");
+                    Double v1 = scanner.nextDouble();
+                    System.out.print("Ingrese valor 2: ");
+                    Double v2 = scanner.nextDouble();
+                    ejecutarOperacion(opcion, v1, v2);
+                }
+            }    
         } while (opcion>=1&&opcion<8);
         System.out.print("Saliendo del programa----Tenga un buen dia");
     }
     
+    private static <T extends Number> void ejecutarOperacion(int opcion, T v1, T v2) {
+        switch (opcion) {
+            case 1 -> System.out.println("Resultado: " + suma(v1, v2));
+            case 2 -> System.out.println("Resultado: " + resta(v1, v2));
+            case 3 -> System.out.println("Resultado: " + producto(v1, v2));
+            case 4 -> System.out.println("Resultado: " + division(v1, v2));
+            case 5 -> System.out.println("Resultado: " + potencia(v1, v2));
+        }
+    }
+     
 }
--- laboratorio3eda/Ejercicio2/Operador.java
+++ laboratorio3eda/Ejercicio2/Operador.java
@@ -9,5 +9,12 @@ public class Operador<T extends Number> {
         this.valor1 = valor1;
         this.valor2 = valor2;
     }
-    
+
+    public T getValor1() {
+        return valor1;
+    }
+
+    public T getValor2() {
+        return valor2;
+    }   
 }
