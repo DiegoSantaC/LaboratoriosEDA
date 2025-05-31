879f53cf0f8a7ef79cecf6686980e38ff7dcebb2
Diego <dsantacruz@unsa.edu.pe>
2025-05-30 19:48:22 -0500

    Agregando ejercicio resuelto 8, utilizando el metodo set para remplar un elemento de mi linkedlist

---
+++ Laboratorio4/EjerciciosResueltos/EjercicioR8.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.Laboratorio4.EjerciciosResueltos;
+import java.util.*;
+
+public class EjercicioR8 {
+
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Uno");
+        ll.add("Dos");
+        ll.add(1, "Tres");
+        System.out.println("Inicial LinkedList " + ll);
+        ll.set(1, "Cuatro");
+        System.out.println("Actualizada LinkedList " + ll);
+    }
+    
+}
