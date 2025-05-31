6723fdcbea46ee720b93cdfadd727b82275a0f33
Diego <dsantacruz@unsa.edu.pe>
2025-05-30 20:33:25 -0500

    Modificando la clase Node para que funcione en la lista doblemente enlazada

--- Laboratorio4/EjerciciosPropuestos/EjercicioP1/DoublyLinkedList.java
+++ Laboratorio4/EjerciciosPropuestos/EjercicioP1/DoublyLinkedList.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.Laboratorio4.EjerciciosPropuestos.EjercicioP1;
 
-/**
- *
- * @author windows
- */
-public class DoublyLinkedList {
-    
+public class DoublyLinkedList<T> {
+    Node<T> root;    
 }
