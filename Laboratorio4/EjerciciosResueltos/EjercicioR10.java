dd5334ffeb4db228c4ba1fb96753e1e554639a40
Diego <dsantacruz@unsa.edu.pe>
2025-05-30 19:58:40 -0500

    Agregando ejercicio resuelto 10, usando el metodo toArray para convertir mi LinkedList en un array y viendo la comparacion de como se imprimen los elementos de cada uno

---
+++ Laboratorio4/EjerciciosResueltos/EjercicioR10.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.Laboratorio4.EjerciciosResueltos;
+import java.util.*;
+
+public class EjercicioR10 {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(123);
+        list.add(12);
+        list.add(11);
+        list.add(1134);
+        System.out.println("LinkedList: " + list);
+        Object[] a=list.toArray();
+        System.out.print("Despues de convertir LinkedList a un Array: ");
+        for(Object e : a){
+            System.out.print(e + " ");
+        }
+    } 
+}
