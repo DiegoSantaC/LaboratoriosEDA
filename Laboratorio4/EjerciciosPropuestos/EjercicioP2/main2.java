2480560375767de2f55b6bd5061979fa35c87027
Diego <dsantacruz@unsa.edu.pe>
2025-05-31 13:47:08 -0500

    Agregando ejercicio propuesto 3, creando una lista doblemente enlazada con java.util

--- Laboratorio4/EjerciciosPropuestos/EjercicioP2/main2.java
+++ Laboratorio4/EjerciciosPropuestos/EjercicioP2/main2.java
@@ -5,8 +5,8 @@ public class main2 {
 
     public static void main(String[] args) {
         CircularLinkedList<Integer> list= new CircularLinkedList<>();
-        for(int i=1 ; i <= 10; i++)
-            list.insertLeft(i);
+        for(int i=1 ; i <= 12; i++)
+            list.insertRight(i);
         System.out.println(list);
     }   
 }
