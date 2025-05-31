3719c8b266006269f4abc9e8cff2b6ba936fa3fa
Diego <dsantacruz@unsa.edu.pe>
2025-05-30 19:12:52 -0500

    Agregando ejercicio resuelto 7, agregar elementos por posicion

---
+++ Laboratorio4/EjerciciosResueltos/EjercicioR7.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.Laboratorio4.EjerciciosResueltos;
+import java.util.*;
+
+public class EjercicioR7 {
+
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<String>();
+        ll.add("Uno");
+        ll.add("Tres");
+        ll.add(1,"Dos");
+        System.out.println(ll);
+    }
+    
+}
