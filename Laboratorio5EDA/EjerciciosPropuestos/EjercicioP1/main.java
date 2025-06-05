69936e676a76bff188160fb97c62c11312517df4
Diego <dsantacruz@unsa.edu.pe>
2025-06-05 16:10:33 -0500

    Reutilizando codigo del ejercicio Resuelto 1 al propuesto 1, implementando la Pila con elementos del 1-10, añadiendo una interfaz Stack para reutilizar

---
+++ Laboratorio5EDA/EjerciciosPropuestos/EjercicioP1/main.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.Laboratorio5EDA.EjerciciosResueltos.EjercicioR1;
+
+public class main {
+
+    public static void main(String[] args) {
+        StackLinkedList<Integer> pila=new StackLinkedList<>();
+        for(int i=1 ; i<=8 ; i++)
+            pila.push(i);
+        pila.printList();
+        pila.pop();
+        pila.pop();
+        pila.pop();      
+        pila.printList();
+    }
+    
+}
