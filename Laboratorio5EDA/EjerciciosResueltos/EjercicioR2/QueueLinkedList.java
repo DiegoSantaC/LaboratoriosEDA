255c6ded4317205faacc2b4308d79dd57fd9521b
Diego <dsantacruz@unsa.edu.pe>
2025-06-05 15:54:20 -0500

    Agregando la cola implementada como una linkedlist, reutilizando la clase node(avances en clase)

---
+++ Laboratorio5EDA/EjerciciosResueltos/EjercicioR2/QueueLinkedList.java
@@ -0,0 +1,58 @@
+
+package com.mycompany.Laboratorio5EDA.EjerciciosResueltos.EjercicioR2;
+
+public class QueueLinkedList<E> {
+    private Node<E> primero;
+        private Node<E> ultimo;
+        private int count;
+
+        public QueueLinkedList(){
+            this.primero=null;
+            this.ultimo=null;
+            this.count=0;
+        }
+
+        public void enqueue(E x) {
+            Node<E> nuevoNodo = new Node(x, null);
+            if (isEmpty()) {
+                this.primero=nuevoNodo;
+                this.ultimo=nuevoNodo;
+                primero.setNext(ultimo);
+            } else {
+                this.ultimo.setNext(nuevoNodo);
+                this.ultimo=nuevoNodo;
+            }
+            this.count ++;
+        }
+        
+        public void dequeue(){
+            if (count == 1) {
+                primero=null;
+                ultimo=null;
+                count--;
+                }else if(!isEmpty()){
+                    primero=primero.getNext();
+                    count--;
+                } else 
+                    System.out.print("La cola esta vacia, no se puede desencolar");
+        }
+
+        public void printList(){
+        System.out.println("Mi cola es: "+ toString());
+        System.out.println("El primero es: "+ primero.getData());
+        System.out.println("El ultimo es: "+ ultimo.getData());
+        }
+
+        @Override
+        public String toString() {
+            String str = "";
+            for(Node<E> aux = this.primero; aux != null; aux = aux.getNext())
+                    str += aux.toString() + ", ";
+            return str;
+        }
+
+        public boolean isEmpty(){
+            return primero==null;
+        }
+    
+}
--- Laboratorio5EDA/EjerciciosResueltos/EjercicioR2/main.java
+++ Laboratorio5EDA/EjerciciosResueltos/EjercicioR2/main.java
@@ -4,6 +4,13 @@ package com.mycompany.Laboratorio5EDA.EjerciciosResueltos.EjercicioR2;
 public class main {
 
     public static void main(String[] args) {
+        QueueLinkedList<Integer> cola=new QueueLinkedList<>();
+        for(int i=1 ; i<=10 ; i++)
+            cola.enqueue(i);
+        cola.printList();
+        cola.dequeue();
+        cola.dequeue();
+        cola.printList();
     }
     
 }
