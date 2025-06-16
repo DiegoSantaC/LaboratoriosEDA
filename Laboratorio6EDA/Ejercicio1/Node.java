e8c43eb7fd99b5cb2d250e05672889d297e7621d
Diego <dsantacruz@unsa.edu.pe>
2025-06-16 15:41:32 -0500

    -------------Empezando el laboratorio 6, creando las clases necesarias para mi ejercicio 1---------------

---
+++ Laboratorio6EDA/Ejercicio1/Node.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Laboratorio6EDA.Ejercicio1;
+
+/**
+ *
+ * @author windows
+ */
+public class Node {
+    
+}
