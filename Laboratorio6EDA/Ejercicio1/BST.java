ada1ed4fbc39d0696d38ad900bc55d1048f1cd63
Diego <dsantacruz@unsa.edu.pe>
2025-06-16 15:54:15 -0500

    Implementando atributos y el metodo insert para mi clase BST, tambien agregando un exception para cuando se inserta un elemento duplicado

--- Laboratorio6EDA/Ejercicio1/BST.java
+++ Laboratorio6EDA/Ejercicio1/BST.java
@@ -1,6 +1,36 @@
 
 package com.mycompany.Laboratorio6EDA.Ejercicio1;
 
-public class BST {
+public class BST<E extends Comparable<E>> {
+    protected Node<E> root;
+    protected int count;
     
+    public BST(){
+        this.root=null;
+        this.count=0;
+    }
+    public BST(Node<E> node){
+        this.root=node;
+        this.count=1;
+    }
+    
+    public void insert(E x) throws ItemDuplicated{
+        this.root=insertRecursivo(x, root);
+    }
+    
+    public Node<E> insertRecursivo(E x, Node<E> actual) throws ItemDuplicated{
+        Node<E> res=actual;
+        if(actual==null)
+            res=new Node<E>(x);
+        else{
+            int valor=x.compareTo(actual.getData());
+            if(valor<0)
+                actual.setLeft(insertRecursivo(x, actual.getLeft()));
+            else if(valor==0)
+                throw new ItemDuplicated(x+", ya se encuentra en el arbol");
+            else
+                actual.setRight(insertRecursivo(x, actual.getRight()));               
+        }
+        return res;
+    }
 }
---
+++ Laboratorio6EDA/Ejercicio1/ItemDuplicated.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.Laboratorio6EDA.Ejercicio1;
+
+public class ItemDuplicated extends Exception {
+
+    public ItemDuplicated() {
+    }
+    
+    public ItemDuplicated(String msg) {
+        super(msg);
+    }
+}
