69c94a914f9a1e367177733271b85f56024fe9bb
Diego <dsantacruz@unsa.edu.pe>
2025-07-01 22:45:52 -0500

    Implementando la clase NodeAVL

--- Laboratorio7EDA/NodeAVL.java
+++ Laboratorio7EDA/NodeAVL.java
@@ -1,6 +1,38 @@
 
 package Laboratorio7EDA;
 
-public class NodeAVL {
+public class NodeAVL<E> {
+    NodeAVL<E> left;
+    NodeAVL<E> right;
+    private int FE;
+
+    public NodeAVL(E data) {
+        this.left=null;
+        this.right=null;
+        this.FE=0;
+    }
+     
+    public NodeAVL<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(NodeAVL<E> left) {
+        this.left = left;
+    }
     
+    public NodeAVL<E> getRight() {
+        return right;
+    }
+
+    public void setRight(NodeAVL<E> right) {
+        this.right = right;
+    }
+    
+    public int getFE() {
+        return FE;
+    }
+
+    public void setFE(int FE) {
+        this.FE = FE;
+    }
 }
