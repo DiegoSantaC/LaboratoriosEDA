258aadcfc8391275f0d8078969bfe83c20309d66
Diego <dsantacruz@unsa.edu.pe>
2025-07-01 22:48:35 -0500

    Implementando en mi clase AVL los atributos, constructor y el metodo para conseguir la altura

--- Laboratorio7EDA/ALV.java
+++ Laboratorio7EDA/ALV.java
@@ -1,6 +1,37 @@
 
 package Laboratorio7EDA;
 
-public class ALV {
+public class ALV<E extends Comparable<E>> {
+    private NodeAVL<E> root;
+    private int count;
+    
+    public ALV(){
+        this.root = null;
+        count=0;
+    }
+    public ALV(NodeAVL<E> root){
+        this.root = root;
+        count=1;
+    }
+    
+    public void altura(){
+        System.out.println("La altura de mi arbol es: "+ altura(root));
+    }
+    
+    private int altura(NodeAVL<E> actual){
+        if(actual!=null){
+            int alturaIzquierda=0;
+            int alturaDerecha=0;
+            if(actual.getLeft()!=null)
+                alturaIzquierda+=altura(actual.getLeft());
+            alturaIzquierda++;
+            if(actual.getRight()!=null)
+                alturaDerecha+=altura(actual.getRight());
+            alturaDerecha++;
+
+            return Math.max(alturaIzquierda, alturaDerecha);
+        } else
+            return 0;
+    }
     
 }
