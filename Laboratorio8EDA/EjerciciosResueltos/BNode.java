3d6606bf340a85286382578fd612ca03966cb717
Diego <dsantacruz@unsa.edu.pe>
2025-07-08 18:39:44 -0500

    Implementado ejercicio resuelto sin completar

---
+++ Laboratorio8EDA/EjerciciosResueltos/BNode.java
@@ -0,0 +1,36 @@
+
+package Laboratorio8EDA.EjerciciosResueltos;
+import java.util.*;
+
+public class BNode<E> {
+    protected ArrayList<E> keys;
+    protected ArrayList<BNode<E>> childs;
+    protected int count;
+    public BNode(int n){
+        this.keys = new ArrayList<E>(n);
+        this.childs = new ArrayList<BNode<E>>(n);
+        this.count = 0;
+        for (int i = 0; i < n; i++){
+            this.keys.add(null);
+            this.childs.add(null);
+        }
+    }
+    // Check if the current node is full
+    public boolean nodeFull (int i){
+    
+    }
+    // Check if the current node is empty
+    public boolean nodeEmpty (){
+    
+    }
+    //Search for a key in the current node, if found it returns true and
+    //the position where it is located, otherwise, returns false and the
+    //position of the child where it should descend.
+    public boolean searchNode (E data, int[] pos){
+    
+    }
+    // Return the keys found in the node.
+    public String toString(){
+    
+    }
+}
