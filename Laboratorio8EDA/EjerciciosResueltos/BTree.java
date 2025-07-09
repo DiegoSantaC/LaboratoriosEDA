0daf2aaeed6f5956e839b0b25316cd09dc23aa61
Diego <dsantacruz@unsa.edu.pe>
2025-07-08 22:47:08 -0500

    Terminando aunque sea el ejercicio resuelto

--- Laboratorio8EDA/EjerciciosResueltos/BTree.java
+++ Laboratorio8EDA/EjerciciosResueltos/BTree.java
@@ -1,21 +1,21 @@
 
 package Laboratorio8EDA.EjerciciosResueltos;
 
-public class BTree<E extends Comparable<E>>{
+public class BTree<E extends Comparable<E>> {
     private BNode<E> root;
     private int orden;
     private boolean up;
     private BNode<E> nDes;
-    
+
     public BTree(int orden) {
         this.orden = orden;
         this.root = null;
     }
-    
+
     public boolean isEmpty() {
         return this.root == null;
     }
-    
+
     public void insert(E cl) {
         up = false;
         E mediana;
@@ -30,7 +30,7 @@ public class BTree<E extends Comparable<E>>{
             this.root = pnew;
         }
     }
-    
+
     private E push(BNode<E> current, E cl) {
         int pos[] = new int[1];
         E mediana;
@@ -39,8 +39,7 @@ public class BTree<E extends Comparable<E>>{
             nDes = null;
             return cl;
         } else {
-            boolean fl;
-            fl = current.searchNode(cl, pos);
+            boolean fl = current.searchNode(cl, pos);
             if (fl) {
                 System.out.println("Item duplicado\n");
                 up = false;
@@ -48,17 +47,19 @@ public class BTree<E extends Comparable<E>>{
             }
             mediana = push(current.childs.get(pos[0]), cl);
             if (up) {
-                if (current.nodeFull(this.orden - 1))
+                if (current.nodeFull(this.orden - 1)) {
                     mediana = dividedNode(current, mediana, pos[0]);
-                else { }
+                } else {
+                    putNode(current, mediana, nDes, pos[0]);
+                    up = false;
                 }
-                up = false;
-                putNode(current, mediana, nDes, pos[0]);
                 return mediana;
+            } else {
+                return null;
             }
-    } 
- 
-    
+        }
+    }
+
     private void putNode(BNode<E> current, E cl, BNode<E> rd, int k) {
         int i;
         for (i = current.count - 1; i >= k; i--) {
@@ -69,39 +70,63 @@ public class BTree<E extends Comparable<E>>{
         current.childs.set(k + 1, rd);
         current.count++;
     }
-    
+
     private E dividedNode(BNode<E> current, E cl, int k) {
         BNode<E> rd = nDes;
         int i, posMdna;
         posMdna = (k <= this.orden / 2) ? this.orden / 2 : this.orden / 2 + 1;
+
         nDes = new BNode<E>(this.orden);
+
+        // Copiar al nuevo nodo derecho
         for (i = posMdna; i < this.orden - 1; i++) {
-            nDes.keys.set(posMdna, current.keys.get(i));
+            nDes.keys.set(i - posMdna, current.keys.get(i));
             nDes.childs.set(i - posMdna + 1, current.childs.get(i + 1));
         }
+
         nDes.count = (this.orden - 1) - posMdna;
         current.count = posMdna;
-        if (k <= this.orden / 2)
+
+        if (k <= this.orden / 2) {
             putNode(current, cl, rd, k);
-        else
+        } else {
             putNode(nDes, cl, rd, k - posMdna);
+        }
+
         E median = current.keys.get(current.count - 1);
         nDes.childs.set(0, current.childs.get(current.count));
         current.count--;
+
         return median;
     }
-    
-    public String toString(){
-        String s = "";
-        if (isEmpty())
-            s += "BTree is empty...";
-        else
-            s = writeTree(this.root);
-        return s;
+
+    public String toString() {
+        if (isEmpty()) return "BTree is empty...";
+        return writeTree(this.root, 0);
     }
-    
-    private String writeTree(BNode<E> current){
-    
+
+    private String writeTree(BNode<E> current, int level) {
+        StringBuilder sb = new StringBuilder();
+
+        // Sangría por nivel
+        for (int i = 0; i < level; i++) {
+            sb.append("    ");
+        }
+
+        sb.append("[ ");
+        for (int i = 0; i < current.count; i++) {
+            sb.append(current.keys.get(i)).append(" ");
+        }
+        sb.append("]\n");
+
+        // Recursivo por hijos
+        for (int i = 0; i <= current.count; i++) {
+            if (current.childs.get(i) != null) {
+                sb.append(writeTree(current.childs.get(i), level + 1));
+            }
+        }
+
+        return sb.toString();
     }
 }
  
