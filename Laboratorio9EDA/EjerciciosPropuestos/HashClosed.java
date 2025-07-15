61bedf90f41b13a8257034d0b376b511cd51bc00
Diego <dsantacruz@unsa.edu.pe>
2025-07-15 18:53:37 -0500

    Implementado mi HashClosed, creando el metodo de insert y printTable

--- Laboratorio9EDA/EjerciciosPropuestos/HashClosed.java
+++ Laboratorio9EDA/EjerciciosPropuestos/HashClosed.java
@@ -1,6 +1,42 @@
 
 package Laboratorio9EDA.EjerciciosPropuestos;
 
-public class HashClosed {
-    
+public class HashClosed<E> {
+    private Register<E>[] table;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public HashClosed(int capacity) {
+        table = new Register[capacity];
+        size = 0;
+    }
+
+    private int hash(int key) {
+        return Math.abs(key) % table.length;
+    }
+
+    public boolean insert(Register<E> reg) {
+        int index = hash(reg.getKey());
+        int originalIndex = index;
+        do {
+            if (table[index] == null || table[index].isDeleted()) {
+                table[index] = reg;
+                size++;
+                return true;
+            }
+            if (table[index].getKey() == reg.getKey() && !table[index].isDeleted()) {
+                System.out.println("Clave duplicada");
+                return false;
+            }
+            index = (index + 1) % table.length;
+        } while (index != originalIndex);
+        return false; // si la tabla esta llena
+    }
+
+    public void printTable() {
+        System.out.println("\n--- Tabla Hash Cerrada ---");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println("[" + i + "] " + (table[i] == null ? "[VACÍO]" : table[i]));
+        }
+    }
 }
---
+++ Laboratorio9EDA/EjerciciosPropuestos/TestHashClosed.java
@@ -0,0 +1,10 @@
+
+package Laboratorio9EDA.EjerciciosPropuestos;
+
+public class TestHashClosed {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
